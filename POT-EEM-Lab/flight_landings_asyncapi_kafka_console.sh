7f4b90d67cf07ad60c2f9a748913b64477459388
chechusv <57540245+chechusv@users.noreply.github.com>
2024-11-13 14:05:49 +0200

    Update flight_landings_asyncapi_kafka_console.sh

--- POT-EEM-Lab/flight_landings_asyncapi_kafka_console.sh
+++ POT-EEM-Lab/flight_landings_asyncapi_kafka_console.sh
@@ -4,7 +4,7 @@ EGW_API_CLIENTID=ad492007-b2ed-4dd3-8221-88a8f210dfce
 EGW_APIKEY=64bc7ab1127b74672b285db55d5ab870
 EGW_APISECRET=9f62716b30e529f52dda46fe7d1cc787
 
-~/kafka_2.13-3.2.1/bin/kafka-console-consumer.sh --bootstrap-server $EGW_BOOTSTRAP_SERVER \
+~/kafka_2.13-3.7.1/bin/kafka-console-consumer.sh --bootstrap-server $EGW_BOOTSTRAP_SERVER \
   --group "1" \
   --consumer-property "client.id=$EGW_API_CLIENTID" \
   --key-deserializer "org.apache.kafka.common.serialization.StringDeserializer" \
@@ -15,4 +15,5 @@ EGW_APISECRET=9f62716b30e529f52dda46fe7d1cc787
   --consumer-property "ssl.truststore.location=./egw-cert.p12" \
   --consumer-property "ssl.truststore.password=passw0rd" \
   --consumer-property "ssl.truststore.type=PKCS12" \
+  --consumer-property "ssl.endpoint.identification.algorithm=" \
   --topic "FLIGHT.LANDINGS"
